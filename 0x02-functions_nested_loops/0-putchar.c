2a1311b77b2b1554a0ece6dfeebc1cae64e4bbe1
Days-OG <a07025425@itesm.mx>
2021-10-18 18:55:55 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+char *holberton;
+for (holberton = "_putchar\n"; *holberton != '\0'; ++holberton)
+	_putchar(*holberton);
+return (0);
+}
