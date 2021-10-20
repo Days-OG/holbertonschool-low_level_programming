8fb6b0caf30bef78ca6182b6aa0cff868c9da35a
Days-OG <a07025425@itesm.mx>
2021-10-19 18:30:34 -0700

    commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,13 +4,13 @@
  * _isalpha - check if a character is alphabethical
  * @c: the character to check
  *
- * Retur: 1 if c is alphabethical
+ * Return: 1 if c is alphabethical
  * Otherwise 0.
  */
 
 int _isalpha(int c)
 {
-if ((c >= 'a' && c <= 'z') || ( c >= && c <= 'Z'))
+if ((c >= 'a' && c <= 'z') || ( c >= 'A' && c <= 'Z'))
 {
 return (1);
 }
