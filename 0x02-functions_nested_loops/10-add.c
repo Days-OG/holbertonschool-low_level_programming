451d62bf6ef40fe93d426e1275750648aaa4f124
Days-OG <a07025425@itesm.mx>
2021-10-21 16:41:52 -0700

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two integer
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: the sum of a and b
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
