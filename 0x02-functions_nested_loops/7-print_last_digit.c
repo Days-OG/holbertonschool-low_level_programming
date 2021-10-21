f34686a53e7c3ec43e648cbf486173e9ccb446f8
Days-OG <a07025425@itesm.mx>
2021-10-21 16:19:24 -0700

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a integer
+ * @n: the value to print the last digit of
+ *
+ * Return: the last digit of n
+ */
+
+int print_last_digit(int n)
+{
+n = n % 10;
+if ( n < 0)
+{
+n = -1 * n;
+}
+_putchar('0' + n);
+return (n);
+}
