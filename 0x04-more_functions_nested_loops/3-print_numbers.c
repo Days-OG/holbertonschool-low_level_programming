7791d7e3f7066f1415e8dfe1f08b1981544c9565
Days-OG <a07025425@itesm.mx>
2021-10-24 05:45:17 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers
+ * @a: first multiplication operand
+ * @b: second multiplication operan
+ *
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
