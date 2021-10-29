e4c13e0ff0dde8f601b9f2dbf31b66c5c629e8cb
Days-OG <a07025425@itesm.mx>
2021-10-28 22:38:51 -0700

    commit

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -4,8 +4,8 @@
  * _putchar - write the character c to stdout
  * @c: the character to  print
  *
- * Return: on success 1
- * on error, -1 is returned, and error is set appropriately
+ * Return: On success 1
+ * On error, -1 is returned, and error is set appropriately
  */
 
 int _putchar(char c)
