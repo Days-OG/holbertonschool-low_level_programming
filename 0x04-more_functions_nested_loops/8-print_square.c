3d9163d377beba9354de3eb79892fb6427ad1dd4
Days-OG <a07025425@itesm.mx>
2021-10-28 21:24:54 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - print a square in the terminal
+ * @sie: size of square
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+int linenum;
+int linelen;
+
+for (linenum = 0; linenum < size; ++linenum)
+{
+for (linelen = 0; linelen < size; ++linelen)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+}
