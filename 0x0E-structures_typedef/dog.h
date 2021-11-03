6646cd4a0c7479b8849dd36147d06331f71fe330
Days-OG <a07025425@itesm.mx>
2021-11-02 21:43:30 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,20 +11,15 @@
  * @owner: the name of the dog's owner
  */
 
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-/**
- * dog - rename struct with typedef
- */
-
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
