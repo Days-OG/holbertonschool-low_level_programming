372443089d2e039ca29f2e95ac31da23c2e1090b
Days-OG <a07025425@itesm.mx>
2021-11-11 20:50:37 -0800

    commit

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-files=(*.c)
+files=( *.c )
 gcc -c -Wall -Werror -Wextra -pedantic "${files[@]}"
-ar -rc liball.a "${files[@]/%.c/.o]"
-rm "${files[@]/%.c/.o]"
+ar -rc liball.a "${files[@]/%.c/.o}"
+rm "${files[@]/%.c/.o}"
