6387f9431319546d96cb0d57763be4faa26ec8de
Days-OG <a07025425@itesm.mx>
2021-11-14 17:02:03 -0800

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * str_concat - concatenate two strings
  * @s1: a pointer to the first string
@@ -8,17 +7,14 @@
  * Return: NULL if memory allocation fails
  * otherwise a pointer to a the new string
  */
-
 char *str_concat(char *s1, char *s2)
 {
 unsigned int len1 = 0;
 unsigned int len2 = 0;
 char *cat;
-
 if (s1)
 {
 while (s1[len1])
-{
 ++len1;
 }
 else
@@ -28,17 +24,13 @@ s1 = "";
 if (s2)
 {
 while (s2[len2])
-{
 ++len2;
-}
 }
 else
 {
 s2 = "";
 }
-
 cat = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
-
 if (!cat)
 {
 return (NULL);
@@ -46,16 +38,12 @@ return (NULL);
 if (s1)
 {
 for (len1 = 0; s1[len1]; ++len1)
-{
 cat[len1] = s1[len1];
-}
 }
 if (s2)
 {
 for (len2 = 0; s2[len2]; ++len2)
-{
 cat[len1 + len2] = s2[len2];
-}
 }
 cat[len1 + len2] = '\0';
 return (cat);
