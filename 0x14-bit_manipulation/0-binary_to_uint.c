c949a3b824ba7480c73b452d4697e8f35e0eaa47
Days-OG <a07025425@itesm.mx>
2021-11-19 03:20:15 -0800

    commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_unit - convert a binary string to an integer
+ * binary_to_uint - convert a binary string to an integer
  * @b: the string to convert
  *
  * Return: the converted number or 0 if b contains a character
