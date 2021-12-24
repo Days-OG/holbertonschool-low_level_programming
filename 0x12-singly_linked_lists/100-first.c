9af762d5b3635dfffa03dd64314580423d3c7e30
root <a07025425@itesm.mx>
2021-12-23 21:41:11 -0800

    commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,7 @@ void init(void)
 {
 printf("%s\n%s\n",
 "You're beat! and yet, ypu must allow,",
-"I bore my house upon my back!"
+"I bore my house upon my back!",
+"(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)"
 );
 }
