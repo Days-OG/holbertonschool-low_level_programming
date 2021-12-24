67467e66bbb0302433ea393efcc4e6f356dc90ac
root <a07025425@itesm.mx>
2021-12-23 21:42:30 -0800

    commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@ void init(void) __attribute__((constructor));
  */
 void init(void)
 {
-printf("%s\n%s\n",
+printf("%s\n%s\n%s",
 "You're beat! and yet, ypu must allow,",
 "I bore my house upon my back!",
 "(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)"
