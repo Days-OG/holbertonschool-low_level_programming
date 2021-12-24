26c9cdde59d0afa89030ebf345a430ad3dfb3d04
root <a07025425@itesm.mx>
2021-12-23 21:49:40 -0800

    grammar

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@ void init(void) __attribute__((constructor));
 void init(void)
 {
 printf("%s\n%s\n",
-"You're beat! and yet, ypu must allow,",
+"You're beat! and yet, you must allow,",
 "I bore my house upon my back!"
 );
 }
